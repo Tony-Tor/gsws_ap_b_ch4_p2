1d6dbabcc6ae1deb306ee0ced65d76d474319216
Tony-Tor <Tony.Tor2016@yandex.ru>
2023-07-22 13:23:15 +0400

    ex2 добавление Spring создание бинов через @Bean и DI через @Autowired

--- src/main/java/main/ProjectConfig.java
+++ src/main/java/main/ProjectConfig.java
@@ -1,9 +1,30 @@
 package main;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import proxies.CommentNotificationProxy;
+import proxies.EmailCommentNotificationProxy;
+import repositories.CommetRepository;
+import repositories.DBCommentRepository;
+import services.CommentService;
 
 @Configuration
-@ComponentScan(basePackages = {"proxies","repositories","services"})
 public class ProjectConfig {
+    @Bean
+    public CommetRepository commetRepository(){
+        return new DBCommentRepository();
+    }
+
+    @Bean
+    public CommentNotificationProxy commentNotificationProxy(){
+        return new EmailCommentNotificationProxy();
+    }
+
+    @Bean
+    public CommentService commentService(CommetRepository commentRepository,
+                                         CommentNotificationProxy commentNotificationProxy){
+        return new CommentService(commentRepository,
+                commentNotificationProxy);
+    }
 }
--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -1,13 +1,16 @@
 package services;
 
 import main.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import proxies.CommentNotificationProxy;
 import repositories.CommetRepository;
 
 @Component
 public class CommentService {
+    @Autowired
     private final CommetRepository commetRepository;
+    @Autowired
     private final CommentNotificationProxy commentNotificationProxy;
 
     public CommentService(CommetRepository commetRepository, CommentNotificationProxy commentNotificationProxy) {

1b0d961d91057ce77aba5122593e0f58d912693a
Tony-Tor <Tony.Tor2016@yandex.ru>
2023-07-22 13:18:40 +0400

    ex2 добавление Spring

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,18 +1,19 @@
 package main;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import proxies.EmailCommentNotificationProxy;
 import repositories.DBCommentRepository;
 import services.CommentService;
 
 public class Main {
     public static void main(String[] args) {
-        var commentRepository = new DBCommentRepository();
-        var commentNotificationProxy = new EmailCommentNotificationProxy();
-        var commentService = new CommentService(commentRepository, commentNotificationProxy);
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
         var comment = new Comment();
         comment.setAuthor("Tony");
         comment.setText("Demo comment");
+
+        var commentService = context.getBean(CommentService.class);
 
         commentService.publishComment(comment);
     }
--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -1,9 +1,11 @@
 package services;
 
 import main.Comment;
+import org.springframework.stereotype.Component;
 import proxies.CommentNotificationProxy;
 import repositories.CommetRepository;
 
+@Component
 public class CommentService {
     private final CommetRepository commetRepository;
     private final CommentNotificationProxy commentNotificationProxy;
